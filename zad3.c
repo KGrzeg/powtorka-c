e3c8a58eab2a2eff356eebcc26677861a3ff0ce3
Grzegorz Kupczyk <kupczykgrzeg@gmail.com>
2020-12-07 22:13:07 +0100

    zad 3

---
+++ zad3.c
@@ -0,0 +1,29 @@
+#include <math.h>
+#include <stdio.h>
+
+struct Wynik {
+  double sin;
+  double cos;
+  double tg;
+};
+
+struct Wynik angles(double angle){
+  angle = (angle / 360) * M_PI_2; //zamiana stopni na radiany
+
+  struct Wynik ret;
+  ret.sin = sin(angle);
+  ret.cos = cos(angle);
+  ret.tg = tan(angle);
+
+  return ret;  
+};
+
+int main(int argc, char const *argv[])
+{
+  const float angle = 30;
+  struct Wynik funkcje = angles(angle);
+
+  printf("kat: %lf\nsin: %lf\n cos: %lf\n tg: %lf\n", angle, funkcje.sin, funkcje.cos, funkcje.tg);
+
+  return 0;
+}
